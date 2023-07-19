13101e400474551965199eb3a9207106d1e2fb29
shata <shatalova.o.s@yandex.ru>
2023-07-20 00:44:12 +0300

    Users

--- main-service/src/main/java/ru/practicum/ewm/controller/user/UserController.java
+++ main-service/src/main/java/ru/practicum/ewm/controller/user/UserController.java
@@ -7,6 +7,9 @@ import ru.practicum.ewm.dto.user.UserResponse;
 import ru.practicum.ewm.dto.user.UserDto;
 import ru.practicum.ewm.service.UserService;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/admin/users")
 @AllArgsConstructor
@@ -14,14 +17,22 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public void getUser(){
-
+    public List<UserDto> getUser(@RequestParam List<Long> ids,
+                        @RequestParam(defaultValue = "0") Integer from,
+                        @RequestParam(defaultValue = "10") Integer size){
+        return service.getUsers(ids,from,size);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public UserDto addUser(@RequestBody UserResponse userResponse){
         return service.addUser(userResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable long id){
+        service.deleteUser(id);
     }
 }
 
--- main-service/src/main/java/ru/practicum/ewm/service/UserService.java
+++ main-service/src/main/java/ru/practicum/ewm/service/UserService.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.dto.user.UserResponse;
 import ru.practicum.ewm.dto.user.UserDto;
 import ru.practicum.ewm.exceptions.ConflictException;
+import ru.practicum.ewm.exceptions.StorageException;
 import ru.practicum.ewm.exceptions.ValidationException;
 import ru.practicum.ewm.model.User;
 import ru.practicum.ewm.repository.user.UserRepository;
 
 import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Data
@@ -32,10 +37,35 @@ public class UserService {
         } catch (DataIntegrityViolationException ex) {
             throw new ConflictException("Нарушение целостности данных");
         }
-        return   UserDto.builder()
+        return toUserDto(user);
+    }
+
+    public List<UserDto> getUsers(List<Long> ids, Integer from, Integer size) {
+        if (from < 0 || size < 0) {
+            throw new IllegalArgumentException("Запрос составлен некорректно");
+        }
+        List<User> users = new ArrayList<>();
+        if (ids == null){
+            users = repository.findAll(PageRequest.of(from/size, size)).toList();
+        } else {
+            users = repository.findAllById(ids);
+        }
+        return users.stream()
+                .map(user -> toUserDto(user))
+                .collect(Collectors.toList());
+
+    }
+
+    private UserDto toUserDto(User user){
+        return UserDto.builder()
                 .email(user.getEmail())
                 .id(user.getId())
                 .name(user.getName())
                 .build();
+    }
+
+    public void deleteUser(long id) {
+        User user = repository.findById(id).orElseThrow(()-> new StorageException("Пользователь не найден или недоступен"));
+            repository.deleteById(id);
     }
 }
