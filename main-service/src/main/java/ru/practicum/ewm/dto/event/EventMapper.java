5d615529a9769d025adf10bcf9818ede7198f16d
shata <shatalova.o.s@yandex.ru>
2023-07-21 18:28:55 +0300

    add EventMapper

--- main-service/src/main/java/ru/practicum/ewm/dto/event/EventMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/event/EventMapper.java
@@ -7,9 +7,55 @@ import ru.practicum.ewm.dto.category.CategoryDto;
 import ru.practicum.ewm.dto.user.UserDto;
 import ru.practicum.ewm.dto.user.UserShortDto;
 import ru.practicum.ewm.model.Event;
+import ru.practicum.ewm.model.Location;
+import ru.practicum.ewm.model.ParticipationRequest;
+import ru.practicum.ewm.model.category.Category;
+import ru.practicum.ewm.model.user.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class EventMapper {
+    public static Event toEvent(NewEventDto request, Category category, LocalDateTime now, User user, Location location){
+        return Event.builder()
+                .annotation(request.getAnnotation())
+                .category(category)
+                .requests(new ArrayList<>())
+                .createdOn(now)
+                .description(request.getDescription())
+                .eventDate(request.getEventDate())
+                .initiator(user)
+                .location(location)
+                .paid(request.isPaid())
+                .participantLimit(request.getParticipantLimit())
+                .publishedOn(null)
+                .requestModeration(request.getRequestModeration())
+                .state("PENDING")
+                .title(request.getTitle())
+                .views(0)
+                .build();
+    }
+    public static Event toEvent(NewEventDto request, Category category, List<ParticipationRequest> requests, LocalDateTime now, User user, Location location){
+        return Event.builder()
+                .annotation(request.getAnnotation())
+                .category(category)
+                .requests(requests)
+                .createdOn(now)
+                .description(request.getDescription())
+                .eventDate(request.getEventDate())
+                .initiator(user)
+                .location(location)
+                .paid(request.isPaid())
+                .participantLimit(request.getParticipantLimit())
+                .publishedOn(null)
+                .requestModeration(request.getRequestModeration())
+                .state("PENDING")
+                .title(request.getTitle())
+                .views(0)
+                .build();
+    }
     public static EventFullDto toEventFullDto(Event event, Integer confirmedRequests,CategoryDto categoryDto,UserShortDto userDto,LocationDto locationDto){
         return EventFullDto.builder()
                 .annotation(event.getAnnotation())
--- main-service/src/main/java/ru/practicum/ewm/service/EventService.java
+++ main-service/src/main/java/ru/practicum/ewm/service/EventService.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.practicum.ewm.dto.LocationDto;
@@ -10,12 +9,11 @@ import ru.practicum.ewm.dto.category.CategoryDto;
 import ru.practicum.ewm.dto.event.EventFullDto;
 import ru.practicum.ewm.dto.event.EventMapper;
 import ru.practicum.ewm.dto.event.NewEventDto;
-import ru.practicum.ewm.dto.user.UserDto;
 import ru.practicum.ewm.dto.user.UserShortDto;
 import ru.practicum.ewm.model.Event;
 import ru.practicum.ewm.model.Location;
-import ru.practicum.ewm.model.ParticipationRequest;
 import ru.practicum.ewm.model.category.Category;
+import ru.practicum.ewm.model.user.User;
 import ru.practicum.ewm.repository.EventRepository;
 import ru.practicum.ewm.repository.LocationRepository;
 import ru.practicum.ewm.repository.ParticipationRequestRepository;
@@ -24,7 +22,6 @@ import ru.practicum.ewm.repository.user.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Data
@@ -44,29 +41,13 @@ public class EventService {
         if(request.getRequestModeration() == null){
             request.setRequestModeration(true);
         }
+        User user = userRepository.findById(userId).orElseThrow();
         Location location = locationRepository.save(mapper.map(request.getLocation(),Location.class));
-        Event event = Event.builder()
-                .annotation(request.getAnnotation())
-                .category(category)
-                .requests(new ArrayList<>())
-                .createdOn(now)
-                .description(request.getDescription())
-                .eventDate(request.getEventDate())
-                .initiator(userRepository.findById(userId).orElseThrow())
-                .location(location)
-                .paid(request.isPaid())
-                .participantLimit(request.getParticipantLimit())
-                .publishedOn(null)
-                .requestModeration(request.getRequestModeration())
-                .state("PENDING")
-                .title(request.getTitle())
-                .views(0)
-                .build();
-        Event store = repository.save(event);
+        Event event = repository.save(EventMapper.toEvent(request, category, now, user, location));
         Integer confirmedRequests = (requestRepository.findAllByState("APPROVED")).size();
-        CategoryDto categoryDto = mapper.map(store.getCategory(), CategoryDto.class);
-        UserShortDto userDto = mapper.map(store.getInitiator(),UserShortDto.class);
-        LocationDto locationDto = mapper.map(store.getLocation(), LocationDto.class);
-        return EventMapper.toEventFullDto(store, confirmedRequests,categoryDto, userDto, locationDto);
+        CategoryDto categoryDto = mapper.map(event.getCategory(), CategoryDto.class);
+        UserShortDto userDto = mapper.map(event.getInitiator(),UserShortDto.class);
+        LocationDto locationDto = mapper.map(event.getLocation(), LocationDto.class);
+        return EventMapper.toEventFullDto(event, confirmedRequests,categoryDto, userDto, locationDto);
     }
 }
