b58e929ae9ad2579fc97bca91e66e7a8ab5c8734
shata <shatalova.o.s@yandex.ru>
2023-08-01 00:21:42 +0300

    add exceptions

--- main-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -1,7 +1,9 @@
 package ru.practicum.ewm.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,23 +13,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
     public ErrorResponse handleValidationException(final ValidationException e) {
-        log.info("Ошибка 400");
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleChangeException(final StorageException e) {
-        log.info("Ошибка 404");
+        log.debug("Получен статус 404 Not found {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflictException(final ConflictException e) {
-        log.info("Ошибка 409");
+        log.debug("Получен статус 409 Conflict {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.debug("Получен статус 500 Interval server error {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 }
